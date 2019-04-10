b616b88b047deb68c2ec60b342292b34b4e3ea43
exbracer <exbracer@gmail.com>
2019-04-10 17:57:22 +0900

    init hello.py

---
+++ hello.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python
+# encoding: utf-8
+# coding style: pep8
+# ====================================================
+#   Copyright (C)2019 All rights reserved.
+#
+#   Author        : bbxytl
+#   Email         : bbxytl@gmail.com
+#   File Name     : hello.py
+#   Last Modified : 2019-04-10 17:55
+#   Describe      :
+#
+# ====================================================
+
+import sys
+# import os
+
+
